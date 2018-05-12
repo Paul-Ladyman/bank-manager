ffae8e3523eec6bc20651e8952141ce8dfa0f4ea
Paul-Ladyman <ladyman.paul@gmail.com>
2018-05-12 15:54:57 +0100

    Simplify the reporter

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const csv = require("csvtojson");
 const reporter = require('./reporter');
 const statementUtils = require('./statementUtils');
+const breakdownUtils = require('./breakdownUtils');
 
 csv()
   .fromFile('./statement.csv')
   .on("end_parsed", function(statements) { 
     const statementsByMonth = statementUtils.getStatementsByMonth(statements, {});
-    reporter.reportByMonth(statementsByMonth);
+    const months = Object.keys(statementsByMonth);
+
+    months.forEach((month) => {
+      const breakdown = breakdownUtils.getMonthBreakdown(statementsByMonth[month]);
+      reporter.reportByMonth(month, breakdown);
+    });
   })
--- reporter.js
+++ reporter.js
@@ -1,37 +1,30 @@
-const breakdownUtils = require('./breakdownUtils');
+function reportByMonth(month, breakdown) {
+  console.log(`${month}`);
+  const largestDebit = breakdown.largestDebit;
+  
+  if (breakdown.wage) {
+    console.log(`\tWage: ${breakdown.wage}`);
+  }
 
-function reportByMonth(statementsByMonth) {
-  const months = Object.keys(statementsByMonth);
+  if (breakdown.rent) {
+    console.log(`\tRent: ${breakdown.rent}`);
+  }
 
-  months.forEach((month) => {
-    console.log(`${month}`);
-    const breakdown = breakdownUtils.getMonthBreakdown(statementsByMonth[month]);
-    const largestDebit = breakdown.largestDebit;
-    
-    if (breakdown.wage) {
-      console.log(`\tWage: ${breakdown.wage}`);
-    }
+  console.log(`\tLargest debit: ${largestDebit['Transaction Description']} - ${largestDebit['Debit Amount']}`);
 
-    if (breakdown.rent) {
-      console.log(`\tRent: ${breakdown.rent}`);
-    }
+  if (breakdown.credits.length > 0) {
+    console.log('\tCredits:');
+    breakdown.credits.forEach((credit) => {
+      console.log(`\t\t${credit['Transaction Description']} - ${credit['Credit Amount']}`);
+    });
+  }
 
-    console.log(`\tLargest debit: ${largestDebit['Transaction Description']} - ${largestDebit['Debit Amount']}`);
-
-    if (breakdown.credits.length > 0) {
-      console.log('\tCredits:');
-      breakdown.credits.forEach((credit) => {
-        console.log(`\t\t${credit['Transaction Description']} - ${credit['Credit Amount']}`);
-      });
-    }
-
-    if (breakdown.bills.length > 0) {
-      console.log('\tBills:');
-      breakdown.bills.forEach((bill) => {
-        console.log(`\t\t${bill['Transaction Description']} - ${bill['Debit Amount']}`);
-      });
-    }
-  })
+  if (breakdown.bills.length > 0) {
+    console.log('\tBills:');
+    breakdown.bills.forEach((bill) => {
+      console.log(`\t\t${bill['Transaction Description']} - ${bill['Debit Amount']}`);
+    });
+  }
 }
 
 module.exports = {
