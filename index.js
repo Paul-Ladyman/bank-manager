36eabb9cf3dc8e2d8efbd065258ef7d5618f6355
Paul Ladyman <ladyman.paul@gmail.com>
2021-02-08 14:28:16 +0000

    Create graph data from breakdowns and serve as graph HTML page

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const csv = require("csvtojson");
+const fs = require('fs');
 const reporter = require('./reporter');
 const statementUtils = require('./statementUtils');
 const breakdownUtils = require('./breakdownUtils');
@@ -9,8 +10,17 @@ csv()
     const statementsByMonth = statementUtils.getStatementsByMonth(statements, {});
     const months = Object.keys(statementsByMonth);
 
-    months.forEach((month) => {
+    const breakdowns = months.map((month) => {
       const breakdown = breakdownUtils.getMonthBreakdown(statementsByMonth[month]);
-      reporter.reportByMonth(month, breakdown);
+      return [month, breakdown];
     });
+
+    breakdowns.forEach(([month, breakdown]) => reporter.reportByMonth(month, breakdown));
+
+    const data = breakdowns.map(([month, breakdown]) => {
+      const { balanceBeforeWage } = breakdown;
+      return { month, balanceBeforeWage };
+    });
+
+    fs.writeFileSync('./data/data.json', JSON.stringify(data));
   })
