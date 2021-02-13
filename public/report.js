4b9812342b556d3cfd12713edaa42b9dd8405a87
Paul Ladyman <ladyman.paul@gmail.com>
2021-02-13 17:16:30 +0000

    Report styling improvements

--- public/report.js
+++ public/report.js
@@ -9,83 +9,107 @@ function getReport(breakdowns, spendingLimit) {
         report[month] = addition;
       }
     }
-  
-    function indentClass(indent) {
-      return indent ? 'report-item__indented' : '';
+
+    function reportItems(title, items) {
+      addToReport(`<div class="report--title">${title}:</div>`);
+      items.forEach(({description, value, total}) => {
+        const descriptionToRender = total
+          ? `<b>${description}</b>`
+          : description;
+        const valueWithDefault = `£${value || ''}`;
+        const valueToRender = total
+          ? `<b>${valueWithDefault}</b>`
+          : valueWithDefault;
+        addToReport(`<div class="report-item"><div>${descriptionToRender}</div><div>${valueToRender}</div></div>`);
+      });
     }
-  
-    function logCredit(credit, indent) {
-      addToReport(`<div class="report-item report-item__credit ${indentClass(indent)}">${credit}</div>`);
+
+    function reportContainer(title, itemsModifier, items, total) {
+      addToReport(`<div class="report-container"><div class="report-items ${itemsModifier}">`);
+      const itemsToRender = total
+        ? [
+          ...items,
+          { description: 'Total', value: total, total: true }
+        ]
+        : items
+      reportItems(title, itemsToRender);
+      addToReport('</div></div>');
     }
-  
-    function logDebit(debit, indent) {
-      addToReport(`<div class="report-item report-item__debit ${indentClass(indent)}">${debit}</div>`);
+
+    function logHighlight(title, value, itemsModifier) {
+      addToReport(`<div class="highlight  ${itemsModifier}"><div class="highlight--title">${title}:</div><div>£${value || ''}</div></div>`);
     }
-  
-    function logBalance(balance) {
-      addToReport(`<div class="report-item report-item__balance">${balance}</div>`);
-    }
-  
-    function logWarning(warning, indent) {
-      addToReport(`<div class="report-item report-item__warning ${indentClass(indent)}">${warning}</div>`);
+
+    function reportHighlights(highlights, itemsModifier) {
+      addToReport(`<div class="highlights">`);
+      highlights.forEach(({description, value}) => logHighlight(description, value, itemsModifier))
+      addToReport('</div>');
     }
 
     addToReport(`<h2>${month}</h2>`);
 
-    if (breakdown.balanceBeforeWage) {
-      logBalance(`Balance Before Wage: £${breakdown.balanceBeforeWage}`);
-    }
+    addToReport('<div class="report-body--columns"><div class="report-body--left">');
 
-    if (breakdown.savingsTotal) {
-      logCredit(`Savings: £${breakdown.savingsTotal}`);
-    }
-
-    if (breakdown.wage) {
-      logCredit(`Wage: £${breakdown.wage}`);
-    }
+    reportHighlights([
+      { description: 'Balance Before Wage', value: breakdown.balanceBeforeWage },
+      { description: 'Savings', value: breakdown.savingsTotal },
+      { description: 'Wage', value: breakdown.wage },
+      { description: 'Final Balance', value: breakdown.finalBalance },
+    ], 'report-items__credit');
 
     if (breakdown.credits.length > 0) {
-      logCredit('Other Credits:');
-      breakdown.credits.forEach((credit) => {
-        logCredit(`${credit['Transaction Description']} - £${credit['Credit Amount']}`, true);
-      });
+      reportContainer('Credits', 'report-items__credit', breakdown.credits.map((credit) => ({
+        description: credit['Transaction Description'],
+        value: credit['Credit Amount']
+      })));
     }
 
-    if (breakdown.rent) {
-      logDebit(`Rent: £${breakdown.rent}`);
-    }
+    reportHighlights([
+      { description: 'Rent', value: breakdown.rent },
+      { description: 'Transport', value: breakdown.transportTotal.toFixed(2) },
+    ], 'report-items__debit');
 
-    if (breakdown.transportTotal) {
-      logDebit(`Transport: £${breakdown.transportTotal.toFixed(2)}`);
-    }
+    reportContainer(
+      'Largest Debit (excl. rent and savings)',
+      'report-items__debit',
+      [{ description: breakdown.largestDebit['Transaction Description'], value: breakdown.largestDebit['Debit Amount'] }]
+    );
 
     if (breakdown.utilities.length > 0) {
-      logDebit('Utilities:');
-      breakdown.utilities.forEach((utility) => {
-        logDebit(`${utility['Transaction Description']} - £${utility['Debit Amount']}`, true);
-      });
-      logDebit(`Total - £${breakdown.utilitiesTotal.toFixed(2)}`, true);
+      reportContainer(
+        'Utilities',
+        'report-items__debit',
+        breakdown.utilities.map((utility) => ({
+          description: utility['Transaction Description'],
+          value: utility['Debit Amount']
+        })),
+        breakdown.utilitiesTotal.toFixed(2)
+      );
     }
 
     if (breakdown.bills.length > 0) {
-      logDebit('Other Bills:');
-      breakdown.bills.forEach((bill) => {
-        logDebit(`${bill['Transaction Description']} - £${bill['Debit Amount']}`, true);
-      });
-      logDebit(`Total - £${breakdown.billsTotal.toFixed(2)}`, true);
+      reportContainer(
+        'Other Bills',
+        'report-items__debit',
+        breakdown.bills.map((utility) => ({
+          description: utility['Transaction Description'],
+          value: utility['Debit Amount']
+        })),
+        breakdown.billsTotal.toFixed(2)
+      );
     }
+
+    addToReport('</div><div class="report-body--right">');
 
     if (breakdown.spendingWarnings.length > 0) {
-      logWarning(`Spending Warnings (> £${parseFloat(spendingLimit)}):`);
-      breakdown.spendingWarnings.forEach((warning) => {
-        logWarning(`${warning['Transaction Description']} - £${warning['Debit Amount']}`, true);
-      });
+      const warningsTitle = `Spending Warnings (> £${parseFloat(spendingLimit)})`;
+      reportItems(warningsTitle, breakdown.spendingWarnings.map((warning) => ({
+        description: warning['Transaction Description'],
+        value: warning['Debit Amount']
+      })));
     }
 
-    logDebit(`Largest Debit (excl. rent and savings):`);
-    logDebit(`${breakdown.largestDebit['Transaction Description']} - £${breakdown.largestDebit['Debit Amount']}`, true);
-
-    logBalance(`Final Balance: £${breakdown.finalBalance}`);
+    addToReport('</div></div>');
   }
 
   breakdowns.forEach(({month, breakdown}) => reportByMonth(month, breakdown, spendingLimit));
