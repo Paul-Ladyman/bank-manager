e34a00364c43b69fb31db2e3029ce8ae478fdaf4
Paul Ladyman <ladyman.paul@gmail.com>
2022-05-07 11:16:11 +0200

    Add mapper for new caisse d'epargne statement schema

--- statementMapper.js
+++ statementMapper.js
@@ -2,7 +2,11 @@ function isCaisseDepargne(file) {
   return file.includes('caissedepargne');
 }
 
-function mapCaisseDepargneStatements(originalStatements, balance, mappedStatements = []) {
+function isOldCaisseDepargne(file) {
+  return file.includes('caissedepargne.old');
+}
+
+function mapCaisseDepargneStatementsOld(originalStatements, balance, mappedStatements = []) {
   if (originalStatements.length === 0) {
     const header = 'Transaction Date,Transaction Type,Transaction Description,Debit Amount,Credit Amount,Balance\n';
     return `${header}${mappedStatements.join('\n')}`;
@@ -40,19 +44,84 @@ function mapCaisseDepargneStatements(originalStatements, balance, mappedStatemen
     ...mappedStatements,
     mappedStatement
   ];
+  return mapCaisseDepargneStatementsOld(otherStatements, newBalance, newMappedStatements);
+}
+
+function mapCaisseDepargneOld(rawStatements) {
+  const lines = rawStatements.split('\n');
+  const finalBalanceLine = lines[3];
+  const finalBalance = finalBalanceLine.replace(/.*?(\d*),(\d*)/, '$1.$2');
+  const originalStatements = lines.slice(5, lines.length - 1);
+  return mapCaisseDepargneStatementsOld(originalStatements, finalBalance);
+}
+
+function sortCaisseDepargneStatements(statements) {
+  return statements.sort((statement1, statement2) => {
+    const [_, day1, month1, year1] = statement1.match(/(\d\d)\/(\d\d)\/(\d\d\d\d),/);
+    const [__, day2, month2, year2] = statement2.match(/(\d\d)\/(\d\d)\/(\d\d\d\d),/);
+    const dateObj1 = new Date(`${month1}/${day1}/${year1}`);
+    const dateObj2 = new Date(`${month2}/${day2}/${year2}`);
+    if (dateObj1 > dateObj2) {
+      return -1;
+    }
+
+    if (dateObj1 < dateObj2) {
+      return 1;
+    }
+
+    return 0;
+  });
+}
+
+function mapCaisseDepargneStatements(originalStatements, balance, mappedStatements = []) {
+  if (originalStatements.length === 0) {
+    return mappedStatements;
+  }
+
+  const [statement, ...otherStatements] = originalStatements;
+  const [
+    _,
+    date,
+    label,
+    amount
+  ] = statement.match(/.*?,(.*?),.*?,.*?,(.*?),(.*?),/);
+  const balanceNumber = parseFloat(balance);
+  const amountNumber = parseFloat(amount);
+  const newBalance = balanceNumber - amountNumber;
+  const absoluteDebitAmount = amount.includes('-') ? amount.replace('-', '') : '';
+  const absoluteCreditAmount = amount.includes('+') ? amount.replace('+', '') : '';
+
+  const [___, year, month, day] = date.match(/(\d\d\d\d)-(\d\d)-(\d\d)/);
+  const standardDate = `${day}/${month}/${year}`;
+
+  const mappedStatement = `${standardDate},UN,${label},${absoluteDebitAmount},${absoluteCreditAmount},${balanceNumber.toFixed(2)}`;
+  const newMappedStatements = [
+    ...mappedStatements,
+    mappedStatement
+  ];
   return mapCaisseDepargneStatements(otherStatements, newBalance, newMappedStatements);
 }
 
 function mapCaisseDepargne(rawStatements) {
   const lines = rawStatements.split('\n');
-  const finalBalanceLine = lines[3];
-  const finalBalance = finalBalanceLine.replace(/.*?(\d*),(\d*)/, '$1.$2');
-  const originalStatements = lines.slice(5, lines.length - 1);
-  return mapCaisseDepargneStatements(originalStatements, finalBalance);
+  const finalBalance = lines[0];
+  const originalStatements = lines.slice(2, lines.length - 1);
+  const mappedStatements = mapCaisseDepargneStatements(originalStatements, finalBalance);
+  const sortedStatements = sortCaisseDepargneStatements(mappedStatements);
+  const header = 'Transaction Date,Transaction Type,Transaction Description,Debit Amount,Credit Amount,Balance\n';
+  return `${header}${sortedStatements.join('\n')}`;
 }
 
 function mapStatements(file, rawStatements) {
-  return isCaisseDepargne(file) ? mapCaisseDepargne(rawStatements) : rawStatements;
+  if (isOldCaisseDepargne(file)) {
+    return mapCaisseDepargneOld(rawStatements);
+  }
+
+  if (isCaisseDepargne(file)) {
+    return mapCaisseDepargne(rawStatements);
+  }
+
+  return rawStatements;
 }
 
 module.exports = mapStatements;
